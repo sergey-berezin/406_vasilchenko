ee3d944a7d36b2b1d1a9a15345aca24c7fe860d3
Demosfen Vasilchenko <dvasil.arm@gmail.com>
2024-09-12 19:41:28 +0300

    first commit

---
+++ GeneticAlg/GeneticAlg/Program.cs
@@ -0,0 +1,112 @@
+﻿using RouteOptimizerLib;
+
+static double[,] arrayInput()
+{
+    Console.Write("Cities count:");
+    int citiesCount = Convert.ToInt32(Console.ReadLine());
+    double[,] res = new double[citiesCount, citiesCount];
+    for (int i = 0; i < citiesCount; i++)
+    {
+        string tmp = Console.ReadLine();
+        string[] values = tmp.Split(' ');
+        for (int j = 0; j < values.Length; j++)
+        {
+            res[i, j] = Convert.ToDouble(values[j]);
+        }
+    }
+    return res;
+}
+static List<int> citiesInput()
+{
+    List<int> ints = new List<int>();
+    string tmp  = Console.ReadLine();
+    string[] strs = tmp.Split(' ');
+    for (int i = 0; i < strs.Length; i++)
+    {
+        ints.Add(int.Parse(strs[i]));
+    }
+    return ints;
+}
+static void printMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+static void printList(List<int> list)
+{
+    foreach(int i in list)
+    {
+        Console.Write(i + " ");
+    }
+}
+static void runWithInput()
+{
+    Console.WriteLine("Input distance matrix:");
+    double[,] matrix = arrayInput();
+    Console.WriteLine("Input cities that you want to visit");
+    List<int> cities = citiesInput();
+    Console.Write("Input population size:");
+    int populationSize = int.Parse(Console.ReadLine());
+    Console.Write("Input epoch count:");
+    int epochCount = int.Parse(Console.ReadLine());
+     runAlgorithm(matrix,cities, populationSize, epochCount);
+
+}
+static void runAlgorithm(double[,] M, List<int> selectedCities, int populationSize, int epochCount)
+{
+    RouteOptimizer routeOptimizer = new RouteOptimizer(M.GetLength(0), M, selectedCities);
+    Route best = null;
+    routeOptimizer.StartAlgorithm(populationSize);
+
+    for(int i = 0;i < epochCount; i++)
+    {
+        best  = routeOptimizer.NextIteration();
+        Console.WriteLine($"Epoch#{i + 1}: best length: {best.Distance} on route: {best.ToString()}");
+    }
+    Console.WriteLine($"Best route:{best} with length: {best.Distance}");
+}
+static void runDefault()
+{
+    double[,] M =
+{
+    {0, 6, 4, 8, 7, 14 },
+    {6, 0, 7, 11, 7, 10 },
+    {4, 7, 0, 4, 3, 10 },
+    {8, 11, 4, 0, 5, 11 },
+    {7, 7, 3, 5, 0, 7 },
+   { 14, 10, 10, 11, 7, 0 }
+};
+    List<int> cities = new List<int>() { 0, 1, 2, 3, 4, 5 };
+    Console.WriteLine("Matrix:");
+    printMatrix(M);
+    Console.WriteLine("Selected cities:");
+    printList(cities);
+    int populationSize = 100;
+    int epochCount = 30;
+    Console.WriteLine($"Population size: {populationSize}   Epoch count: {epochCount}");
+    runAlgorithm(M, cities, populationSize, epochCount);
+}
+
+
+Console.WriteLine("0 - default; 1 - custom");
+char c = Console.ReadLine()[0];
+switch (c)
+{
+    case '0':
+        runDefault();
+        break;
+    case '1':
+        runWithInput();
+        break;
+}
+
+
+Console.ReadLine();
+
+
---
+++ GeneticAlg/RouteOprimizerLib/RouteOptimizer.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Reflection.Metadata.Ecma335;
+
+namespace RouteOptimizerLib
+{
+    public class RouteOptimizer
+    {
+        double[,] DistanceMatrix;
+        int cityCount;
+        List<int> selectedCities;
+        Population currentPopulation;
+        int populationSize = 0;
+        Random random = new Random();
+        public RouteOptimizer(int cityCount, double[,] Matrix, List<int> selectedCities)
+        {
+            this.cityCount = cityCount;
+            this.selectedCities = selectedCities;
+            this.DistanceMatrix = Matrix;
+        }
+        public RouteOptimizer() 
+        {
+            DistanceMatrix = new double[0, 0];
+            cityCount = 0;
+            selectedCities = new List<int>();
+        }
+
+        public Route Calculate(int populationSize = 100, int generationsCount = 5)
+        {
+            currentPopulation = CreateInitialPopulation(selectedCities, populationSize);
+            for (int i = 0; i < generationsCount; i++)
+            {
+
+                Population nextPopulation = new Population();
+                List<double> scores = getFitnessScores(currentPopulation);
+                Route bestRoute = getBestRoute(currentPopulation, scores);
+                nextPopulation.Add(bestRoute);
+                for (int j = 0; j < populationSize / 2 - 1; j++)
+                {
+                    var parents = SelectParents(currentPopulation, scores);
+                    Route child1 = Crossover(parents.Item1, parents.Item2);
+                    Route child2 = Crossover(parents.Item2, parents.Item1);
+
+                    child1.Mutate(random);
+                    child2.Mutate(random);
+
+                    nextPopulation.Add(child1);
+                    nextPopulation.Add(child2);
+                }
+                currentPopulation = nextPopulation;
+            }
+
+            List<double> finalFitnessScores = getFitnessScores(currentPopulation);
+            int bestRouteIndex = finalFitnessScores.IndexOf(finalFitnessScores.Max());
+            Route Route = currentPopulation[bestRouteIndex];
+            double length = Route.CalculateDistance(DistanceMatrix);
+            return Route;
+        } 
+        public void StartAlgorithm(int populationSize = 100)
+        {
+            this.populationSize = populationSize;
+            currentPopulation = CreateInitialPopulation(selectedCities, populationSize);
+        }
+        public Route NextIteration()
+        {
+            Population nextPopulation = new Population();
+            List<double> scores = getFitnessScores(currentPopulation);
+            Route bestRoute = getBestRoute(currentPopulation, scores);
+            nextPopulation.Add(bestRoute);
+            for (int j = 0; j < populationSize / 2 - 1; j++)
+            {
+                var parents = SelectParents(currentPopulation, scores);
+                Route child1 = Crossover(parents.Item1, parents.Item2);
+                Route child2 = Crossover(parents.Item2, parents.Item1);
+
+                child1.Mutate(random);
+                child2.Mutate(random);
+
+                nextPopulation.Add(child1);
+                nextPopulation.Add(child2);
+            }
+            currentPopulation = nextPopulation;
+            return getBestRoute(currentPopulation, getFitnessScores(currentPopulation));
+        }
+        private Route getBestRoute(Population p, List<double> scores)
+        {
+            Route bestRoute = p[scores.IndexOf(scores.Max())];
+            return bestRoute;
+        }
+
+        private List<double> getFitnessScores(Population population) 
+        {
+            List<double> scores = new List<double>();
+            foreach(Route r in population)
+            {
+                scores.Add(1 / r.CalculateDistance(DistanceMatrix));
+            }
+            return scores;
+        }
+        public Tuple<Route, Route> SelectParents(Population population, List<double> fitnessScores)
+        {
+            double totalFitness = fitnessScores.Sum();
+            List<double> selectionProbs = fitnessScores.Select(f => f / totalFitness).ToList();
+
+            var parent1 = population[SelectByProbability(selectionProbs)];
+            var parent2 = population[SelectByProbability(selectionProbs)];
+
+            return Tuple.Create(parent1, parent2);
+        }
+        public int SelectByProbability(List<double> probs)
+        {
+            double r = random.NextDouble();
+            double cumulative = 0.0;
+            for (int i = 0; i < probs.Count; i++)
+            {
+                cumulative += probs[i];
+                if (r < cumulative)
+                    return i;
+            }
+            return probs.Count - 1;
+        }
+
+
+        public Route Crossover(Route parent1, Route parent2)
+        {
+            int cut = random.Next(1, parent1.Count - 1);
+            var child = parent1.Take(cut);
+
+            for(int i = 0;i < parent2.Count; i++)
+            {
+                if (!child.Contains(parent2[i]))
+                {
+                    child.Add(parent2[i]);
+                }
+            }
+            return new Route(child); ;
+        }
+
+
+        private Population CreateInitialPopulation(List<int> basicCities, int populationSize)
+        {
+            List<Route> routes = new List<Route>();
+            for(int i= 0;i < populationSize;i++)
+            {
+                routes.Add(createRandomRouteWithSelectedCities(basicCities, cityCount));
+            }
+            return new Population( routes);
+        }
+        private Route createRandomRouteWithSelectedCities(List<int> basicCities, int cityCount)
+        {
+            int unselectedCitiesCount = random.Next(cityCount - basicCities.Count + 1);
+            int index = 0;
+            int citiesAdded = 0;
+            List<int> cities = new List<int>(basicCities);
+            while(unselectedCitiesCount > 0)
+            {
+                if (basicCities.Contains(index)) { index++; continue; }
+                cities.Add(index);
+                unselectedCitiesCount--;
+                index++;
+            }
+            Route res = new Route(cities);
+            int mutationsCount = random.Next(cities.Count);
+            for (int i = 0; i < mutationsCount; i++) res.Mutate(random);
+            return res;
+        }
+
+    }
+    public class Route
+    {
+        List<int> cities;
+        public double Distance
+        {
+            get;set;
+        }
+        public int Count
+        {
+            get { return cities.Count; }
+        }
+
+        public Route(List<int> cities)
+        {
+            this.cities = cities;
+        }
+        public double CalculateDistance(double[,] matrix)
+        {
+            double length = 0;
+            for(int i = 0;i < cities.Count - 1; i++)
+            {
+                length += matrix[cities[i], cities[i + 1]];
+            }
+            length += matrix[cities[0], cities.Last()];
+            Distance = length;
+            return Distance;
+        }
+
+        public List<int> Take(int cut)
+        {
+            return cities.Take(cut).ToList();
+        }
+
+        public int this[int index]
+        {
+            get { return cities[index]; }
+            set { cities[index] = value;}
+        }
+        public void Mutate(Random random)
+        {
+            int i = random.Next(cities.Count);
+            int j = random.Next(cities.Count);
+            SwapCities(i, j);
+        }
+        private void SwapCities(int i, int j)
+        {
+            int a = cities[i];
+            cities[i] = cities[j];
+            cities[j] = a;
+        }
+
+        public override string ToString()
+        {
+            string res = cities[0].ToString();
+            for(int i = 1; i < cities.Count; i++)
+            {
+                res += "->" + cities[i].ToString();
+            }
+            return res;
+        }
+    }
+    public class Population : ObservableCollection<Route>
+    {
+        public Population( List<Route> routes)
+        {
+            foreach (Route r in routes)
+                this.Add(r);
+        }
+        public Population() { }
+
+    }
+}
